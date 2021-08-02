5d4ab2841b4cc540242c325205417b55ed4fb12f
Igor Baiocco <igor64br@gmail.com>
2021-08-02 11:03:24 -0300

    v1.2 - Search Button Added

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+import json
 import tkinter as tk
 from tkinter import END, messagebox
 from pass_gen import generate
+import json
 
-# ---------------------------- COLORS ------------------------------------------#
+# ---------------------------- VARIABLES ------------------------------------------#
+data_file_location = '/home/baiocco/a100DoC_projects/day-29-MyPass/data.json'
+default_login = '@gmail.com'
 RED = '#8D2828'
 LIGHT_COLOR1 = '#ECEFA4'
 LIGHT_COLOR2 = '#D9DD6B'
@@ -18,9 +22,15 @@ def password_fill():
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
-    website = input_web.get()
+    website = input_web.get().upper()
     login = input_login.get()
     password = input_password.get()
+    new_data = {
+        website: {
+            'login': login,
+            'password': password
+        }
+    }
 
     if website == '' or login == '' or password == '':
         messagebox.showinfo(title='Uncompleted Form', message='The form is uncompleted. \nPlease fill all the camps and'
@@ -29,19 +39,42 @@ def save():
         user_confirm = messagebox.askyesno(title=input_web, message=f'Is your data correct? \nLocation: {website} \nLogin: {login} '
                                                                     f'\nPasscode: {password}')
         if user_confirm:
-            website = input_web.get()
-            login = input_login.get()
-            password = input_password.get()
             warning_label.config(text='')
-            """
-            If you want to change the data file name, substitute the 'data.txt' by the name you want. Also remember to 
-            change the 'data.txt' file manually in the application files, or else you will create one new data file.
-            """
-            with open(file='data.txt', mode='a') as data_file:
-                data_file.write(f'{website} | {login} | {password}\n')
-                warning_label.config(text='Data saved successfully!')
-                input_web.delete(first=0, last=END)
-                input_password.delete(first=0, last=END)
+
+            data = new_data
+
+            try:
+                with open(file=data_file_location, mode='r') as data_file:
+                    try:
+                        data = json.load(fp=data_file)
+                        data.update(new_data)
+                    except json.JSONDecodeError:
+                        pass
+            except FileNotFoundError:
+                pass
+            with open(file=data_file_location, mode='w') as data_file:
+                json.dump(obj=data, fp=data_file, indent=4)
+
+            warning_label.config(text='Data saved successfully!')
+            input_web.delete(first=0, last=END)
+            input_password.delete(first=0, last=END)
+
+
+# ---------------------------- UI SETUP ------------------------------- #
+def search():
+    website = input_web.get().upper()
+    try:
+        with open(file=data_file_location, mode='r') as data_file:
+            data = json.load(data_file)
+        try:
+            login = data[website]['login']
+            password = data[website]['password']
+            messagebox.showinfo(title=website.title(), message=f'Login: {login} \nPassword: {password}')
+        except KeyError:
+            messagebox.showinfo(title=website.title(), message='Oops! Apparently there is no website registered with that name')
+    except json.decoder.JSONDecodeError:
+        messagebox.showinfo(title='Oh No!', message='There aren\'t any passwords saved yet. Why don\'t you start '
+                                                    'saving now?')
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -49,7 +82,7 @@ main_screen = tk.Tk()
 main_screen.config(padx=35, pady=40, bg=RED)
 main_screen.title('Password Manager')
 
-logo = tk.PhotoImage(file='logo.png')
+logo = tk.PhotoImage(file='/home/baiocco/a100DoC_projects/day-29-MyPass/logo.png')
 
 canvas = tk.Canvas(width=200, height=200, bg=RED, highlightthickness=0)
 canvas.create_image(100, 100, image=logo)
@@ -58,8 +91,8 @@ canvas.grid(column=2, row=1)
 indicator_web = tk.Label(text='Website:', fg=LIGHT_COLOR2, bg=RED, font=('Comic Sans', 15))
 indicator_web.grid(column=1, row=2)
 
-input_web = tk.Entry(width=47)
-input_web.grid(column=2, row=2, columnspan=2)
+input_web = tk.Entry(width=26)
+input_web.grid(column=2, row=2)
 input_web.focus()
 
 indicator_login = tk.Label(text='Email/Username:', fg=LIGHT_COLOR2, bg=RED, font=('Comic Sans', 15))
@@ -71,7 +104,7 @@ input_login = tk.Entry(width=47)
     You can change the email below to your main email or add a "#" to the beginning of the line to comment it and the
     program will automatically ignore the command line.
 '''
-input_login.insert(END, string='example@gmail.com')
+input_login.insert(END, string=default_login)
 
 
 input_login.grid(column=2, row=3, columnspan=2)
@@ -82,13 +115,16 @@ indicator_password.grid(column=1, row=4)
 input_password = tk.Entry(width=26)
 input_password.grid(column=2, row=4)
 
-generate_password_btn = tk.Button(text='Generate Password', fg=RED, bg=LIGHT_COLOR1, highlightthickness=0)
+generate_password_btn = tk.Button(text='Generate Password', fg=RED, bg=LIGHT_COLOR1, highlightthickness=0, width=16)
 generate_password_btn.config(command=password_fill)
 generate_password_btn.grid(column=3, row=4)
 
 add_btn = tk.Button(text='Add Password', width=45, fg=RED, bg=LIGHT_COLOR1, highlightthickness=0)
 add_btn.config(command=save)
 add_btn.grid(column=2, row=5, columnspan=2)
+
+search_btn = tk.Button(text='Search', fg=RED, bg=LIGHT_COLOR1, highlightthickness=0, command=search, width=16)
+search_btn.grid(column=3, row=2)
 
 warning_label = tk.Label(fg=LIGHT_COLOR2, bg=RED, text='')
 warning_label.grid(column=2, row=6, columnspan=2)
